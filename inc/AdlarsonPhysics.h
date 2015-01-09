21fa0f99a086381b8cc8a00681db126607f19aeb
Patrik Adlarson <patrik.adlarson@gmail.com>
2015-01-09 11:35:37 +0100

    added functions for Dalitz plot and calculating m=invariant masses of final state particles, included all possible permutations gammas, to be used for selecting the final state gamma combinations.

--- inc/AdlarsonPhysics.h
+++ inc/AdlarsonPhysics.h
@@ -8,6 +8,7 @@
 
 #include "GTreeManager.h"
 #include "GH1.h"
+#include "TVector.h"
 
 class	AdlarsonPhysics  : public GTreeManager
 {
@@ -15,8 +16,15 @@ private:
     GH1*	IM_6g;
     GH1*	IM_10g;
 
-    TLorentzVector sixgam_v;
-    TLorentzVector tengam_v;
+    TLorentzVector etapr_sixgam[6];
+    TLorentzVector dir3pi0_sixgam[6];
+
+    TLorentzVector etapr_tengam[10];
+
+    static Int_t perm6g[15][6];
+    static Int_t perm6outof7g[7][6];
+    static Int_t perm6outof8g[28][6];
+    static Int_t perm6outof10g[210][6];
 
 protected:
     virtual Bool_t  Start();
@@ -31,13 +39,19 @@ public:
     virtual Bool_t	Init(const char* configfile);
 
     // calculates IM for n photons
-    double_t    IM_Ng( UInt_t n );
+    Double_t    IM_Ng( UInt_t n );
 
-    // method for 6g analysis
+    // functions specifically related to 6g analysis
     void sixgAnalysis();
-    void tengAnalysis();
+    void GetBest6gCombination();
 
-    void PhysicsResults( const TLorentzVector &g );
+    // functions specifically related to 10g analysis
+    void tengAnalysis();
+    void GetBest10gCombination();
+
+
+    void DalitzPlot( const TLorentzVector *g , Double_t &X, Double_t &Y, Int_t &DP_nr );
+    void m2pi0_metapi0( const TLorentzVector *g, Double_t &m_etapi01, Double_t &m_etapi02, Double_t &m_2pi0 );
 
 };
 #endif
