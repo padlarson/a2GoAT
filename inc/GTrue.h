c05bff99797e93c46eca89536a48dc5cd267b3e1
Patrik Adlarson <patrik.adlarson@gmail.com>
2015-01-16 20:43:46 +0100

    object which handles true generated pluto data

---
+++ inc/GTrue.h
@@ -0,0 +1,88 @@
+#ifndef __GTrue_h__
+#define __GTrue_h__
+
+#include "GTreeManager.h"
+#include "GTreePluto.h"
+#include "TLorentzVector.h"
+#include "GH1.h"
+
+#define GTrue_MaxEntries  16
+
+class GTrue
+{
+
+private:
+    GH1                 vX;
+    Double_t            TrueBeamEnergy;
+    // weight of each event, default weight = 1
+    Double_t            weight;
+    // LorentzVectors of all produced true particles;
+    TLorentzVector     neutron;
+    TLorentzVector     proton;
+    TLorentzVector     etaprime;
+    TLorentzVector     omega;
+    TLorentzVector     eta;
+    TLorentzVector     chpi[GTrue_MaxEntries];
+    TLorentzVector     pi0[GTrue_MaxEntries];
+    TLorentzVector     gamma[GTrue_MaxEntries];
+    TLorentzVector     electron;
+    TLorentzVector     positron;
+    // Incrementing nr of each particle type;
+    UInt_t              nchpi;
+    UInt_t              npi0;
+    UInt_t              ngamma;
+    // True vertex
+    TVector3            vertex;
+
+    void                SetWeight(const Double_t w )   { weight = w;}
+
+
+public:
+    GTrue();
+    virtual ~GTrue();
+    void Init(GTreePluto& pluto);
+    void Start(GTreePluto& pluto);
+
+    // proton LorentzVector
+            TLorentzVector& GetTrueProtonLV()           { return proton;}
+    const   TLorentzVector& GetTrueProtonLV()   const   { return proton;}
+    // neutron LorentzVector
+            TLorentzVector& GetTrueNeutronLV()          { return neutron;}
+    const   TLorentzVector& GetTrueNeutronLV()  const   { return neutron;}
+    // eta' LorentzVector
+            TLorentzVector& GetTrueEtaPrimeLV()         { return etaprime;}
+    const   TLorentzVector& GetTrueEtaPrimeLV() const   { return etaprime;}
+    // omega LorentzVector
+            TLorentzVector& GetTrueOmegaLV()            { return omega;}
+    const   TLorentzVector& GetTrueOmegaLV()    const   { return omega;}
+    // eta LorentzVector
+            TLorentzVector& GetTrueEtaLV()              { return eta;}
+    const   TLorentzVector& GetTrueEtaLV()      const   { return eta;}
+    // pi-/+ LorentzVector
+            TLorentzVector& GetTrueChargedPiLV(const Int_t index )       { return chpi[index];}
+    const   TLorentzVector& GetTrueChargedPiLV(const Int_t index ) const { return chpi[index];}
+    // pi0 LorentzVector
+            TLorentzVector& GetTrueNeutralPiLV( Int_t index )       { return pi0[index];}
+    const   TLorentzVector& GetTrueNeutralPiLV( Int_t index ) const { return pi0[index];}
+    // gamma LorentzVector
+            TLorentzVector& GetTrueGammaLV( Int_t index )       { return gamma[index];}
+    const   TLorentzVector& GetTrueGammaLV( Int_t index ) const { return gamma[index];}
+    // electron LorentzVector
+            TLorentzVector& GetTrueElectronLV()       { return electron;}
+    const   TLorentzVector& GetTrueElectronLV() const { return electron;}
+    // positron LorentzVector
+            TLorentzVector& GetTruePositronLV()       { return positron;}
+    const   TLorentzVector& GetTruePositronLV() const { return positron;}
+//
+    Double_t    GetWeight() const   { return weight;}
+
+    UInt_t      GetNchpi()  const   { return nchpi;}
+    UInt_t      GetNpi0()   const   { return npi0;}
+    UInt_t      GetNgamma() const   { return ngamma;}
+
+    // Get Vertices
+    const TVector3&    GetTrueVertex()   const   { return vertex;}
+
+};
+
+#endif //__GTrue_h__
---
+++ src/GTrue.cc
@@ -0,0 +1,83 @@
+#include "GTrue.h"
+#include "GTreePluto.h"
+#include "PParticle.h"
+
+
+GTrue::GTrue():
+    vX("vertexX", "vertexX", 2050, -1025, 1025),
+    weight(1.0),
+    TrueBeamEnergy(-10000.0),
+    vertex(-1000,-1000,-1000),
+    ngamma(0),
+    npi0(0),
+    nchpi(0)
+{
+    weight = 1;
+}
+
+GTrue::~GTrue()
+{
+}
+
+
+void GTrue::Start(GTreePluto& pluto)
+{
+    nchpi = 0;
+    npi0 = 0;
+    ngamma = 0;
+    TrueBeamEnergy = -10000.0;
+
+    vertex  = pluto.GetMCTrue(0)->GetVertex();
+
+    TrueBeamEnergy = pluto.GetMCTrueLV(0).P();
+//    TrueBeamEnergy = pluto.GetAllParticles().front()->P();
+
+    /*for(std::list<PParticle*>::iterator i = pluto.GetAllParticles().begin(); i!=pluto.GetAllParticles().end(); i++)
+    {
+        if((*i)->Is("gamma")){
+            //gamma[ngamma] = (const TLorentzVector*)(*i);
+            ngamma++;
+            continue;
+        }
+    }*/
+
+
+    for(int i = 0; i < pluto.GetAllParticles().size(); i++)
+    {
+        if(pluto.GetMCTrue(i)->Is("n")){
+            neutron = pluto.GetMCTrueLV(i);
+            continue;
+        }
+        if(pluto.GetMCTrue(i)->Is("p")){
+            proton = pluto.GetMCTrueLV(i);
+            continue;
+        }
+        if(pluto.GetMCTrue(i)->Is("eta'")){
+            etaprime = pluto.GetMCTrueLV(i);
+            continue;
+        }
+        if(pluto.GetMCTrue(i)->Is("w")){
+            omega = pluto.GetMCTrueLV(i);
+            continue;
+        }
+        if(pluto.GetMCTrue(i)->Is("eta")){
+            eta = pluto.GetMCTrueLV(i);
+            continue;
+        }
+        if(pluto.GetMCTrue(i)->Is("pi-") || pluto.GetMCTrue(i)->Is("pi+")){
+            chpi[nchpi] = pluto.GetMCTrueLV(i);
+            nchpi++;
+            continue;
+        }
+        if(pluto.GetMCTrue(i)->Is("pi0")){
+            pi0[npi0] = pluto.GetMCTrueLV(i);
+            npi0++;
+            continue;
+        }
+        if(pluto.GetMCTrue(i)->Is("gamma")){
+            gamma[ngamma] = pluto.GetMCTrueLV(i);
+            ngamma++;
+            continue;
+        }
+    }
+}
