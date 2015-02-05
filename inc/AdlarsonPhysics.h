860d73c964f04ad92bccb1cd64e25137484ef442
Patrik Adlarson <patrik.adlarson@gmail.com>
2015-02-05 12:38:54 +0100

    Improved the true and rec analysis

--- inc/AdlarsonPhysics.h
+++ inc/AdlarsonPhysics.h
@@ -17,47 +17,36 @@ class	AdlarsonPhysics  : public GTreeManager
 {
 private:
     // Histograms and scatterplots declared
-    GH1*	IM_6g;
-    GH1*	IM_10g;
 
-    GH1*            True_BeamEnergy;
-    GHistBGSub2*    ThpvsThetaprCM;
-    GHistBGSub2*    ThvE_p;
-    GHistBGSub2*    ThvE_eta_g;
-    GHistBGSub2*    ThvE_pi0_g;
-    GHistBGSub2*    DP_true;
-    GH1*            M_pi1pi2_true;
-    GH1*            M_etapi_true;
+    GH1*            TrueE;
+    GH1*            TrueTh;
+    GH1*            TruePhi;
 
+    GHistBGSub2*    TrueEvTh;
+
+    GHistBGSub2*    dthvdetnr_TAPS;
+    GHistBGSub2*    dfivdetnr_TAPS;
+    GHistBGSub2*    revdetnr_TAPS;
+
+    GHistBGSub2*    dthvdetnr_CB;
+    GHistBGSub2*    dfivdetnr_CB;
+    GHistBGSub2*    revdetnr_CB;
+
+    GHistBGSub2*    Ncl_v_trueEtrueth;
     GHistBGSub2*    EvdE_TAPS;
 
-    // proton identified from TAPS_E vs VETO_dE
-    TCutG*          OpenCutFile(Char_t* filename, Char_t* cutname);
-    TCutG*          cutProtonTAPS;
-    TCutG*          cutPionTAPS;
-    TCutG*          cutElectronTAPS;
 
-    // True LorentzVectors
-    TLorentzVector  eta_true;
-    TLorentzVector  pi01_true;
-    TLorentzVector  pi02_true;
-    TLorentzVector  etapr_true[3];
+    double_t        th_true;
+    double_t        fi_true;
+    double_t        E_true;
+    double_t        th_rec;
+    double_t        fi_rec;
+    double_t        E_rec;
+    double_t        deltath;
+    double_t        deltafi;
+    double_t        REnergy;
 
-    Double_t    Xtrue, Ytrue;
-    Int_t       DPnrTrue;
-    Double_t    m_etapi01True, m_etapi02True, m_2pi0True;
 
-    TLorentzVector etapr_sixgam[6];
-    TLorentzVector dir3pi0_sixgam[6];
-
-    TLorentzVector etapr_tengam[10];
-
-    static Int_t perm6g[15][6];
-    static Int_t perm6outof7g[7][6];
-    static Int_t perm6outof8g[28][6];
-    static Int_t perm6outof10g[210][6];
-
-    GTrue   etapr_6gTrue;
 
 protected:
     virtual Bool_t  Start();
@@ -71,22 +60,6 @@ public:
 
     virtual Bool_t	Init(const char* configfile);
 
-    // calculates IM for n photons
-    Double_t    IM_Ng( UInt_t n );
 
-    // function where true analysis is done for eta' --> eta 2pi0 --> 6g
-    void TrueAnalysis_etapr6g();
-
-    // functions specifically related to 6g analysis
-    void sixgAnalysis();
-    void GetBest6gCombination();
-
-    // functions specifically related to 10g analysis
-    void tengAnalysis();
-    void GetBest10gCombination();
-
-
-    void DalitzPlot( const TLorentzVector g[3] , Double_t &X, Double_t &Y, Int_t &DP_nr );
-    void m2pi0_metapi0(  TLorentzVector g[3], Double_t &m_etapi01, Double_t &m_etapi02, Double_t &m_2pi0 );
 };
 #endif
--- src/AdlarsonPhysics.cc
+++ src/AdlarsonPhysics.cc
@@ -1,31 +1,31 @@
-#include "GParticleReconstruction.h"
+#include "GTreeManager.h"
 #include "AdlarsonPhysics.h"
 #include "GTrue.h"
+#include "GTreeA2Geant.h"
 
 
 
 AdlarsonPhysics::AdlarsonPhysics()
 {
 
-    IM_6g	= new GH1("IM_6g", 	"IM_6g/7g", 240,   0, 1200);
-    IM_10g	= new GH1("IM_10g", "IM_10g", 	240,   0, 1200);
+    TrueE               = new GH1("TrueE", "E_{true}", 200, 0., 1000);
+    TrueTh              = new GH1("TrueTh", "#theta_{true}", 360, 0., 180);
+    TruePhi             = new GH1("TruePhi", "#phi_{true}", 400, -200., 200.);
+    TrueEvTh            = new GHistBGSub2("TrueEvTh", "E_{true} vs #theta_{true}", 100, 0., 1000, 60, 0., 30.);
 
-    // True observables
-        // Beam energy
-        True_BeamEnergy     = new GH1("True_BeamEnergy", "True Beam Energy", 250, 1.400, 1.650);
-        // Phase space final state particles
-        ThpvsThetaprCM      = new GHistBGSub2("ThpvsThetaprCM", "#theta_{p} vs #theta_{#eta^{'}}", 360, 0., 180, 360, 0., 180.);
-        ThvE_p              = new GHistBGSub2("ThvE_p", "E_{p} vs #theta_{p}", 100, 0., 0.6, 100, 0., 25.);
-        ThvE_eta_g          = new GHistBGSub2("ThvE_eta_g", "E_{#gamma, #eta} vs #theta_{#gamma, #eta}", 100, 0, 1.000, 36, 0, 180);
-        ThvE_pi0_g          = new GHistBGSub2("ThvE_pi0_g", "E_{#gamma, #pi^{0}} vs #theta_{#gamma, #pi^{0}}", 60, 0, .600, 36, 0, 180);
-        // Physics result
-        DP_true             = new GHistBGSub2("DP_true", "True Dalitz Plot distribution", 60, -1.5, 1.5, 60, -1.5, 1.5);
-        M_pi1pi2_true       = new GH1("M_pi1pi2_true", "True M_{#pi#pi,true}^{2}", 60, 0.05, 0.20);
-        M_etapi_true        = new GH1("M_etapi_true", "True M_{#eta#pi,true}^{2}", 100, 0.45, 0.70);
+    dthvdetnr_TAPS      = new GHistBGSub2("dthvdetnr_TAPS", "#Delta#theta vs detector nr TAPS", 80, -20., 20, 500, 0, 500);
+    dfivdetnr_TAPS      = new GHistBGSub2("dfivdetnr_TAPS", "#Delta#phi vs detector nr TAPS", 80, -20., 20, 500, 0, 500);
+    revdetnr_TAPS       = new GHistBGSub2("revdetnr_TAPS", "#Delta E /E_{rec} vs detector nr TAPS", 100, -2., 2, 500, 0, 500);
+
+    dthvdetnr_CB        = new GHistBGSub2("dthvdetnr_CB", "#Delta#theta vs detector nr CB", 20, -20., 20, 800, 0, 800);
+    dfivdetnr_CB        = new GHistBGSub2("dfivdetnr_CB", "#Delta#phi vs detector nr CB", 20, -20., 20, 800, 0, 800);
+    revdetnr_CB         = new GHistBGSub2("revdetnr_CB", "#Delta E/E_{rec} vs detector nr CB", 100, -2., 2., 800, 0., 800.);
+
+    Ncl_v_trueEtrueth   = new GHistBGSub2("revdetnr_CB", "#Delta E/E_{rec} vs detector nr CB", 100, -2., 2, 800, 0, 800);
 
 
      // Rec. TAPS - proton analysis
-        EvdE_TAPS      = new GHistBGSub2("EvdE_TAPS", "E vs dE}", 100, 0., 1000, 20, 0., 20.);
+     EvdE_TAPS      = new GHistBGSub2("EvdE_TAPS", "E vs dE", 100, 0., 1000, 100, 0., 20.);
 
 
 
@@ -47,9 +47,7 @@ Bool_t	AdlarsonPhysics::Start()
     }
     SetAsPhysicsFile();
 
-
     TraverseValidEvents();
-
 
 	return kTRUE;
 }
@@ -57,57 +55,58 @@ Bool_t	AdlarsonPhysics::Start()
 void	AdlarsonPhysics::ProcessEvent()
 {
 
+    th_true = (GetGeant()->GetTrueVector(0)).Theta()*TMath::RadToDeg();
+    fi_true = (GetGeant()->GetTrueVector(0)).Phi()*TMath::RadToDeg();
+    E_true  = (GetGeant()->GetTrueVector(0)).E()*1000 - MASS_PROTON;
+//    E_true  = (GetGeant()->GetTrueVector(0)).E()*1000;
 
-    etapr_6gTrue.Start(*GetPluto(), *GetGeant()); // (pluto tree, n part in pluto per event)
- //   if(!GParticleReconstruction::Init())
- //   {
- //       cout << "GParticleReconstruction Init failed!" << endl;
- //       return kFALSE;
- //   }
+    TrueTh->Fill(th_true);
+    TruePhi->Fill(fi_true);
+    TrueE->Fill(E_true);
+    TrueEvTh->Fill(E_true,th_true);
 
+    for (Int_t i = 0; i< GetTracks()->GetNTracks(); i++)
+    {
 
-    TrueAnalysis_etapr6g();
-
- //   if(protons->GetNParticles() > 0)
-        // Strategy is :
-        //  Identify all TAPS hits and check from TOF if the particle is a
-        // nucleon or not. As default all hits are considered to be photons.
-
-        double chi2pr_min = 100000;
-        double chitest = 10000;
-        double factortheta, factorphi;
-        int itrack = -1;
-
-        for (int i = 0; i < GetPhotons()->GetNParticles() ; i++)
+        if( GetTracks()->GetApparatus(i) == GTreeTrack::APPARATUS_CB )
         {
-            if( GetPhotons()->GetApparatus(i) == GTreeTrack::APPARATUS_TAPS )
-            {
-                factortheta = etapr_6gTrue.GetTrueProtonLV().Theta() - GetPhotons()->Particle(i).Theta();
-                factortheta = factortheta*TMath::RadToDeg();
-                factorphi =  etapr_6gTrue.GetTrueProtonLV().Phi() - GetPhotons()->Particle(i).Phi();
-                factorphi = factorphi*TMath::RadToDeg();
-            }
-        }
-        if(itrack != -1)
-        EvdE_TAPS->Fill(GetPhotons()->Particle(itrack).E(), GetPhotons()->GetVetoEnergy(itrack));
+             E_rec  = GetTracks()->GetClusterEnergy(i);
+             th_rec = GetTracks()->GetTheta(i);
+             fi_rec = GetTracks()->GetPhi(i);
 
-        if( (GetPhotons()->GetNParticles() == 6) || (GetPhotons()->GetNParticles() == 7) )
-        {
-            double_t im6g = IM_Ng( GetPhotons()->GetNParticles()  );
-            IM_6g->Fill( im6g );
+             deltath = th_true - th_rec;
+             deltafi = fi_true - fi_rec;
+             if(E_rec > 0.0)
+             REnergy = (E_true-E_rec)/E_rec;
 
-            sixgAnalysis();
+             Double_t itest = GetTracks()->GetCentralCrystal(i);
+
+             dthvdetnr_CB->Fill(deltath, GetTracks()->GetCentralCrystal(i));
+             dfivdetnr_CB->Fill(deltafi, GetTracks()->GetCentralCrystal(i));
+             revdetnr_CB->Fill(REnergy, GetTracks()->GetCentralCrystal(i));
         }
 
-        if(GetPhotons()->GetNParticles() == 10)
+        if( GetTracks()->GetApparatus(i) == GTreeTrack::APPARATUS_TAPS )
         {
-            double_t im10g = IM_Ng( GetPhotons()->GetNParticles()  );
-            IM_10g->Fill( im10g );
+             E_rec  = GetTracks()->GetClusterEnergy(i);
+             th_rec = GetTracks()->GetTheta(i);
+             fi_rec = GetTracks()->GetPhi(i);
 
-            tengAnalysis();
+             deltath = th_true - th_rec;
+             deltafi = fi_true - fi_rec;
+             if(E_rec > 0.0)
+             REnergy = (E_true-E_rec)/E_rec;
+
+             dthvdetnr_TAPS->Fill(deltath, GetTracks()->GetCentralCrystal(i));
+             dfivdetnr_TAPS->Fill(deltafi, GetTracks()->GetCentralCrystal(i));
+             revdetnr_TAPS->Fill(REnergy, GetTracks()->GetCentralCrystal(i));
+
+             EvdE_TAPS->Fill(E_rec, GetTracks()->GetVetoEnergy(i));
         }
 
+    }
 
+   // for int
 
 }
 
@@ -125,370 +124,6 @@ Bool_t	AdlarsonPhysics::Init(const char* configfile)
 
 
 
-void AdlarsonPhysics::TrueAnalysis_etapr6g()
-{
-    True_BeamEnergy->Fill(etapr_6gTrue.GetTrueBeamEnergy());
 
-
-    // calculate Physics
-
-    etapr_true[0] = etapr_6gTrue.GetTrueEtaLV();
-    etapr_true[1] = etapr_6gTrue.GetTrueNeutralPiLV(0);
-    etapr_true[2] = etapr_6gTrue.GetTrueNeutralPiLV(1);
-    DalitzPlot(etapr_true, Xtrue, Ytrue, DPnrTrue);
-    m2pi0_metapi0(etapr_true, m_etapi01True, m_etapi02True, m_2pi0True);
-
-    // calculate True Energy vs Theta of final state particles
-
-    ThvE_p->Fill(etapr_6gTrue.GetTrueProtonLV().E() - MASS_PROTON/1000 ,etapr_6gTrue.GetTrueProtonLV().Theta()*TMath::RadToDeg());
-
-    for(Int_t i = 0; i < etapr_6gTrue.GetNgamma(); i++ )
-    {
-        if ( i > 3 ) // first four gammas come from 2pi0
-            ThvE_eta_g->Fill(etapr_6gTrue.GetTrueGammaLV(i).E(),etapr_6gTrue.GetTrueGammaLV(i).Theta()*TMath::RadToDeg());
-        else
-            ThvE_pi0_g->Fill(etapr_6gTrue.GetTrueGammaLV(i).E(),etapr_6gTrue.GetTrueGammaLV(i).Theta()*TMath::RadToDeg());
-    }
-    return;
-}
-
-
-void AdlarsonPhysics::sixgAnalysis()
-{
-    for( Int_t i = 0; i < GetTagger()->GetNTagged(); i++ )
-    {
-        for ( Int_t j = 0; j < GetProtons()->GetNParticles(); j++ )
-        {
-
-            // run kinematical fit energy and momentum conservation:
-
-            // cut on pdf:
-
-        }
-    }
-
-    // select best combination eta 2pi0 and 3pi0:
-    // GetBest6gCombination();
-
-    // cut in the scatterplot eta 2pi0 3pi0 pdf:
-
-    // run kinfit again now enforcing eta mass (and pi0 pi0 as well?)
-
-    // final results: Dalitz plot, m_pi0pi0, eta prime production as fcn energy
-    // DalitzPlot();
-    // m2pi0_metapi0;
-
-}
-
-void AdlarsonPhysics::tengAnalysis()
-{
-    for( Int_t i = 0; i < GetTagger()->GetNTagged(); i++ )
-    {
-        for ( Int_t j = 0; j < GetProtons()->GetNParticles(); j++ )
-        {
-
-            // run kinematical fit
-            // cut on pdf
-
-
-        }
-    }
-
-    // with the best candidate:
-    // select best combination eta (6g out of 10g, 220 combinations)
-    // cut in the scatterplot eta 2pi0 3pi0 pdf
-
-    // run kinfit again now enforcing eta mass (and pi0 pi0 as well?)
-
-    // final results: Dalitz plot, m_pi0pi0, eta prime production as fcn energy
-    //
-    // DalitzPlot();
-    // m2pi0_metapi0;
-    //
-
-}
-
-
-void AdlarsonPhysics::DalitzPlot(const TLorentzVector g[3] , Double_t &X, Double_t &Y , Int_t &DP_nr)
-{
-
-    // g is the array of TLorentzVectors containing the final event sample in order
-    // 0 1 2 - eta pi01 pi02
-    // Dalitz plot variables
-
-    Double_t T_eta, T_pi1, T_pi2;
-    Double_t Q;
-    Double_t X_max = 1.5, Y_max = 1.5, bin_width = 0.2;
-
-    TVector3 etaprime_rest      = -(g[0]+g[1]+g[2]).BoostVector();
-    TLorentzVector eta_ep_rest  = g[0];
-    TLorentzVector pi01_ep_rest = g[1];
-    TLorentzVector pi02_ep_rest = g[2];
-
-    eta_ep_rest.Boost(etaprime_rest);
-    pi01_ep_rest.Boost(etaprime_rest);
-    pi02_ep_rest.Boost(etaprime_rest);
-
-    T_eta = eta_ep_rest.E() - eta_ep_rest.M();
-    if( pi01_ep_rest.E() > pi02_ep_rest.E() ){
-        T_pi1 = pi01_ep_rest.E() - pi01_ep_rest.M();
-        T_pi2 = pi02_ep_rest.E() - pi02_ep_rest.M();
-    }
-    else{
-        T_pi1 = pi02_ep_rest.E() - pi02_ep_rest.M();
-        T_pi2 = pi01_ep_rest.E() - pi01_ep_rest.M();
-    }
-
-    Q = T_eta + T_pi1 + T_pi2;
-    X = ( TMath::Sqrt(3.0) / Q ) * ( T_pi1 - T_pi2 );
-    Y = ( MASS_ETA + 2 * MASS_PI0 ) / MASS_PI0 * ( T_eta / Q ) - 1.0;
-
-    DP_nr = ( int( X ) + X_max ) / 0.2 + 40*( int( Y ) + Y_max );
-
-    DP_true->Fill(X,Y);
-}
-
-
-Double_t    AdlarsonPhysics::IM_Ng( UInt_t n )
-{
-    TLorentzVector g_vec;
-
-    g_vec.SetPxPyPzE(0,0,0,0);
-    for ( Int_t i = 0 ; i < n; i++ )
-        g_vec += GetPhotons()->Particle(i);
-
-    return g_vec.M();
-}
-
-void AdlarsonPhysics::m2pi0_metapi0(  TLorentzVector g[3], Double_t &m_etapi01, Double_t &m_etapi02, Double_t &m_2pi0 )
-{
-    // g is the array of TLorentzVectors containing the final event sample in order
-    // 0 1 2 - eta pi01 pi02
-    // Dalitz plot variables
-
-    Double_t meta, mpi01, mpi02, metapr;
-
-
-    meta = g[0].M();
-    mpi01 = g[1].M();
-    mpi02 = g[2].M();
-    metapr = ( g[0] + g[1] + g[2] ).M();
-
-    m_etapi01   = ( g[0] + g[1] ).M2();
-    m_etapi02   = ( g[0] + g[2] ).M2();
-    m_2pi0      = ( g[1] + g[2] ).M2();
-
-    M_pi1pi2_true->Fill(m_2pi0);
-    M_etapi_true->Fill(m_etapi01);
-    M_etapi_true->Fill(m_etapi02);
-}
-
-
-
-
-Int_t AdlarsonPhysics::perm6g[15][6]=
-{
-    {0,1, 2,3, 4,5},
-    {0,1, 2,4, 3,5},
-    {0,1, 2,5, 4,3},
-
-    {0,2, 1,3, 4,5},
-    {0,2, 1,4, 3,5},
-    {0,2, 1,5, 4,3},
-
-    {0,3, 2,1, 4,5},
-    {0,3, 2,4, 1,5},
-    {0,3, 2,5, 4,1},
-
-    {0,4, 2,3, 1,5},
-    {0,4, 2,1, 3,5},
-    {0,4, 2,5, 1,3},
-
-    {0,5, 2,3, 4,1},
-    {0,5, 2,4, 3,1},
-    {0,5, 2,1, 4,3}
-};
-
-Int_t AdlarsonPhysics::perm6outof7g[7][6]=
-{
-    { 0, 1, 2, 3, 4, 5 },
-    { 0, 1, 2, 3, 4, 6 },
-    { 0, 1, 2, 3, 5, 6 },
-    { 0, 1, 2, 4, 5, 6 },
-    { 0, 1, 3, 4, 5, 6 },
-    { 0, 2, 3, 4, 5, 6 },
-    { 1, 2, 3, 4, 5, 6 }
-};
-
-Int_t AdlarsonPhysics::perm6outof8g[28][6]=
-{
-    { 0, 1, 2, 3, 4, 5 },   { 0, 1, 2, 3, 4, 6 },   { 0, 1, 2, 3, 4, 7 },
-    { 0, 1, 2, 3, 5, 6 },   { 0, 1, 2, 3, 5, 7 },
-    { 0, 1, 2, 3, 6, 7 },
-
-    { 0, 1, 2, 4, 5, 6 },   { 0, 1, 2, 4, 5, 7 },
-    { 0, 1, 2, 4, 6, 7 },
-
-    { 0, 1, 2, 5, 6, 7 },
-
-    { 0, 1, 3, 4, 5, 6 },   { 0, 1, 3, 4, 5, 7 },
-    { 0, 1, 3, 4, 6, 7 },
-
-    { 0, 1, 3, 5, 6, 7 },
-    { 0, 1, 4, 5, 6, 7 },
-
-    { 0, 2, 3, 4, 5, 6 },   { 0, 2, 3, 4, 5, 7 },
-    { 0, 2, 3, 4, 6, 7 },
-    { 0, 2, 3, 5, 6, 7 },
-    { 0, 2, 4, 5, 6, 7 },
-
-    { 0, 3, 4, 5, 6, 7 },
-
-    { 1, 2, 3, 4, 5, 6 },   { 1, 2, 3, 4, 5, 7 },
-    { 1, 2, 3, 4, 6, 7 },
-    { 1, 2, 3, 5, 6, 7 },
-    { 1, 2, 4, 5, 6, 7 },
-    { 1, 3, 4, 5, 6, 7 },
-
-    { 2, 3, 4, 5, 6, 7 }
-};
-
-Int_t AdlarsonPhysics::perm6outof10g[210][6]=
-{
-    // all possible permutations of 6 from 10 possibilities
-    { 0, 1, 2, 3, 4, 5 }, 	{ 0, 1, 2, 3, 4, 6 }, 	{ 0, 1, 2, 3, 4, 7 }, 	{ 0, 1, 2, 3, 4, 8 }, 	{ 0, 1, 2, 3, 4, 9 },
-    { 0, 1, 2, 3, 5, 6 }, 	{ 0, 1, 2, 3, 5, 7 }, 	{ 0, 1, 2, 3, 5, 8 }, 	{ 0, 1, 2, 3, 5, 9 },
-    { 0, 1, 2, 3, 6, 7 }, 	{ 0, 1, 2, 3, 6, 8 }, 	{ 0, 1, 2, 3, 6, 9 },
-    { 0, 1, 2, 3, 7, 8 }, 	{ 0, 1, 2, 3, 7, 9 },
-    { 0, 1, 2, 3, 8, 9 },
-    { 0, 1, 2, 4, 5, 6 }, 	{ 0, 1, 2, 4, 5, 7 }, 	{ 0, 1, 2, 4, 5, 8 }, 	{ 0, 1, 2, 4, 5, 9 },
-    { 0, 1, 2, 4, 6, 7 }, 	{ 0, 1, 2, 4, 6, 8 }, 	{ 0, 1, 2, 4, 6, 9 },
-    { 0, 1, 2, 4, 7, 8 }, 	{ 0, 1, 2, 4, 7, 9 },
-    { 0, 1, 2, 4, 8, 9 },
-    { 0, 1, 2, 5, 6, 7 }, 	{ 0, 1, 2, 5, 6, 8 }, 	{ 0, 1, 2, 5, 6, 9 },
-    { 0, 1, 2, 5, 7, 8 }, 	{ 0, 1, 2, 5, 7, 9 },
-    { 0, 1, 2, 5, 8, 9 },
-    { 0, 1, 2, 6, 7, 8 }, 	{ 0, 1, 2, 6, 7, 9 },
-    { 0, 1, 2, 6, 8, 9 },
-    { 0, 1, 2, 7, 8, 9 },
-    { 0, 1, 3, 4, 5, 6 }, 	{ 0, 1, 3, 4, 5, 7 }, 	{ 0, 1, 3, 4, 5, 8 }, 	{ 0, 1, 3, 4, 5, 9 },
-    { 0, 1, 3, 4, 6, 7 }, 	{ 0, 1, 3, 4, 6, 8 }, 	{ 0, 1, 3, 4, 6, 9 },
-    { 0, 1, 3, 4, 7, 8 }, 	{ 0, 1, 3, 4, 7, 9 },
-    { 0, 1, 3, 4, 8, 9 },
-    { 0, 1, 3, 5, 6, 7 }, 	{ 0, 1, 3, 5, 6, 8 }, 	{ 0, 1, 3, 5, 6, 9 },
-    { 0, 1, 3, 5, 7, 8 }, 	{ 0, 1, 3, 5, 7, 9 },
-    { 0, 1, 3, 5, 8, 9 },
-    { 0, 1, 3, 6, 7, 8 }, 	{ 0, 1, 3, 6, 7, 9 },
-    { 0, 1, 3, 6, 8, 9 },
-    { 0, 1, 3, 7, 8, 9 },
-    { 0, 1, 4, 5, 6, 7 }, 	{ 0, 1, 4, 5, 6, 8 }, 	{ 0, 1, 4, 5, 6, 9 },
-    { 0, 1, 4, 5, 7, 8 }, 	{ 0, 1, 4, 5, 7, 9 },
-    { 0, 1, 4, 5, 8, 9 },
-    { 0, 1, 4, 6, 7, 8 }, 	{ 0, 1, 4, 6, 7, 9 },
-    { 0, 1, 4, 6, 8, 9 },
-    { 0, 1, 4, 7, 8, 9 },
-    { 0, 1, 5, 6, 7, 8 }, 	{ 0, 1, 5, 6, 7, 9 },
-    { 0, 1, 5, 6, 8, 9 },
-    { 0, 1, 5, 7, 8, 9 },
-    { 0, 1, 6, 7, 8, 9 },
-
-    { 0, 2, 3, 4, 5, 6 }, 	{ 0, 2, 3, 4, 5, 7 }, 	{ 0, 2, 3, 4, 5, 8 }, 	{ 0, 2, 3, 4, 5, 9 },
-    { 0, 2, 3, 4, 6, 7 }, 	{ 0, 2, 3, 4, 6, 8 }, 	{ 0, 2, 3, 4, 6, 9 },
-    { 0, 2, 3, 4, 7, 8 }, 	{ 0, 2, 3, 4, 7, 9 },
-    { 0, 2, 3, 4, 8, 9 },
-    { 0, 2, 3, 5, 6, 7 }, 	{ 0, 2, 3, 5, 6, 8 }, 	{ 0, 2, 3, 5, 6, 9 },
-    { 0, 2, 3, 5, 7, 8 }, 	{ 0, 2, 3, 5, 7, 9 },
-    { 0, 2, 3, 5, 8, 9 },
-    { 0, 2, 3, 6, 7, 8 }, 	{ 0, 2, 3, 6, 7, 9 },
-    { 0, 2, 3, 6, 8, 9 },
-    { 0, 2, 3, 7, 8, 9 },
-    { 0, 2, 4, 5, 6, 7 }, 	{ 0, 2, 4, 5, 6, 8 }, 	{ 0, 2, 4, 5, 6, 9 },
-    { 0, 2, 4, 5, 7, 8 }, 	{ 0, 2, 4, 5, 7, 9 },
-    { 0, 2, 4, 5, 8, 9 },
-    { 0, 2, 4, 6, 7, 8 }, 	{ 0, 2, 4, 6, 7, 9 },
-    { 0, 2, 4, 6, 8, 9 },
-    { 0, 2, 4, 7, 8, 9 },
-    { 0, 2, 5, 6, 7, 8 }, 	{ 0, 2, 5, 6, 7, 9 },
-    { 0, 2, 5, 6, 8, 9 },
-    { 0, 2, 5, 7, 8, 9 },
-    { 0, 2, 6, 7, 8, 9 },
-
-    { 0, 3, 4, 5, 6, 7 }, 	{ 0, 3, 4, 5, 6, 8 }, 	{ 0, 3, 4, 5, 6, 9 },
-    { 0, 3, 4, 5, 7, 8 }, 	{ 0, 3, 4, 5, 7, 9 },
-    { 0, 3, 4, 5, 8, 9 },
-    { 0, 3, 4, 6, 7, 8 }, 	{ 0, 3, 4, 6, 7, 9 },
-    { 0, 3, 4, 6, 8, 9 },
-    { 0, 3, 4, 7, 8, 9 },
-    { 0, 3, 5, 6, 7, 8 }, 	{ 0, 3, 5, 6, 7, 9 },
-    { 0, 3, 5, 6, 8, 9 },
-    { 0, 3, 5, 7, 8, 9 },
-    { 0, 3, 6, 7, 8, 9 },
-    { 0, 4, 5, 6, 7, 8 }, 	{ 0, 4, 5, 6, 7, 9 },
-    { 0, 4, 5, 6, 8, 9 },
-    { 0, 4, 5, 7, 8, 9 },
-    { 0, 4, 6, 7, 8, 9 },
-    { 0, 5, 6, 7, 8, 9 },
-
-    { 1, 2, 3, 4, 5, 6 }, 	{ 1, 2, 3, 4, 5, 7 }, 	{ 1, 2, 3, 4, 5, 8 }, 	{ 1, 2, 3, 4, 5, 9 },
-    { 1, 2, 3, 4, 6, 7 }, 	{ 1, 2, 3, 4, 6, 8 }, 	{ 1, 2, 3, 4, 6, 9 },
-    { 1, 2, 3, 4, 7, 8 }, 	{ 1, 2, 3, 4, 7, 9 },
-    { 1, 2, 3, 4, 8, 9 },
-    { 1, 2, 3, 5, 6, 7 }, 	{ 1, 2, 3, 5, 6, 8 }, 	{ 1, 2, 3, 5, 6, 9 },
-    { 1, 2, 3, 5, 7, 8 }, 	{ 1, 2, 3, 5, 7, 9 },
-    { 1, 2, 3, 5, 8, 9 },
-    { 1, 2, 3, 6, 7, 8 }, 	{ 1, 2, 3, 6, 7, 9 },
-    { 1, 2, 3, 6, 8, 9 },
-    { 1, 2, 3, 7, 8, 9 },
-    { 1, 2, 4, 5, 6, 7 }, 	{ 1, 2, 4, 5, 6, 8 }, 	{ 1, 2, 4, 5, 6, 9 },
-    { 1, 2, 4, 5, 7, 8 }, 	{ 1, 2, 4, 5, 7, 9 },
-    { 1, 2, 4, 5, 8, 9 },
-    { 1, 2, 4, 6, 7, 8 }, 	{ 1, 2, 4, 6, 7, 9 },
-    { 1, 2, 4, 6, 8, 9 },
-    { 1, 2, 4, 7, 8, 9 },
-    { 1, 2, 5, 6, 7, 8 }, 	{ 1, 2, 5, 6, 7, 9 },
-    { 1, 2, 5, 6, 8, 9 },
-    { 1, 2, 5, 7, 8, 9 },
-    { 1, 2, 6, 7, 8, 9 },
-    { 1, 3, 4, 5, 6, 7 }, 	{ 1, 3, 4, 5, 6, 8 }, 	{ 1, 3, 4, 5, 6, 9 },
-    { 1, 3, 4, 5, 7, 8 }, 	{ 1, 3, 4, 5, 7, 9 },
-    { 1, 3, 4, 5, 8, 9 },
-    { 1, 3, 4, 6, 7, 8 }, 	{ 1, 3, 4, 6, 7, 9 },
-    { 1, 3, 4, 6, 8, 9 },
-    { 1, 3, 4, 7, 8, 9 },
-    { 1, 3, 5, 6, 7, 8 }, 	{ 1, 3, 5, 6, 7, 9 },
-    { 1, 3, 5, 6, 8, 9 },
-    { 1, 3, 5, 7, 8, 9 },
-    { 1, 3, 6, 7, 8, 9 },
-    { 1, 4, 5, 6, 7, 8 }, 	{ 1, 4, 5, 6, 7, 9 },
-    { 1, 4, 5, 6, 8, 9 },
-    { 1, 4, 5, 7, 8, 9 },
-    { 1, 4, 6, 7, 8, 9 },
-    { 1, 5, 6, 7, 8, 9 },
-
-    { 2, 3, 4, 5, 6, 7 }, 	{ 2, 3, 4, 5, 6, 8 }, 	{ 2, 3, 4, 5, 6, 9 },
-    { 2, 3, 4, 5, 7, 8 }, 	{ 2, 3, 4, 5, 7, 9 },
-    { 2, 3, 4, 5, 8, 9 },
-    { 2, 3, 4, 6, 7, 8 }, 	{ 2, 3, 4, 6, 7, 9 },
-    { 2, 3, 4, 6, 8, 9 },
-    { 2, 3, 4, 7, 8, 9 },
-    { 2, 3, 5, 6, 7, 8 }, 	{ 2, 3, 5, 6, 7, 9 },
-    { 2, 3, 5, 6, 8, 9 },
-    { 2, 3, 5, 7, 8, 9 },
-    { 2, 3, 6, 7, 8, 9 },
-    { 2, 4, 5, 6, 7, 8 }, 	{ 2, 4, 5, 6, 7, 9 },
-    { 2, 4, 5, 6, 8, 9 },
-    { 2, 4, 5, 7, 8, 9 },
-    { 2, 4, 6, 7, 8, 9 },
-    { 2, 5, 6, 7, 8, 9 },
-
-    { 3, 4, 5, 6, 7, 8 }, 	{ 3, 4, 5, 6, 7, 9 },
-    { 3, 4, 5, 6, 8, 9 },
-    { 3, 4, 5, 7, 8, 9 },
-    { 3, 4, 6, 7, 8, 9 },
-    { 3, 5, 6, 7, 8, 9 },
-
-    { 4, 5, 6, 7, 8, 9 }
-
-};
 
 
